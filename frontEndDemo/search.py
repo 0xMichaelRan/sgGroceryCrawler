53595cfbb5e0143a14d2de611ac5fe6bdd27394a
Michael Ran <theimmortalfish@gmail.com>
2015-06-23 11:49:43 +0800

    support search order (ascending or desc)

--- frontEndDemo/search.py
+++ frontEndDemo/search.py
@@ -32,24 +32,38 @@ class SearchHandler(web.RequestHandler):
 
     def get(self):
 
-		keyword = self.get_argument("keyword", None, True)
+		keyword = self.get_argument("keyword", "", True)
 		sortBy = self.get_argument("sort", "", True)
-		print 'loading search result = ' + keyword + ' sortby ' + sortBy
+		order = self.get_argument("order", "", True)
 
-		if len(keyword) < 3:
+		# check length of keyword
+		if len(keyword.replace(" ", "")) < 3:
 			self.render("index.html", title="Redo your search, please! ")
 		else:
 			keyword = keyword.lower()
+
+		# validate sortBy (default = "key")
+		if sortBy != "now_price" and sortBy != "merchant":
+			sortBy = "key"
+
+		# validate order (default = "1")
+		if order != "-1":
+			order = "1"
+
+		print ('loading search result = ' + keyword + ' sortby ' + sortBy + 
+				" in order of " + order)
 
 		connection = pymongo.MongoClient(MONGODB_SERVER, 27017)
 		db = connection.sg_grocery
 		items = db.table
 
-		if sortBy == 'price':
-			products = items.find({ 'key': { '$regex' :".*" + keyword + ".*"} }).sort([("now_price", 1), ])
-		elif sortBy == 'merchant':
-			products = items.find({ 'key': { '$regex' :".*" + keyword + ".*"} }).sort([("merchant", 1), ])
-		else:
-			products = items.find({ 'key': { '$regex' :".*" + keyword + ".*"} }).sort([("key", 1), ])
+		# search in Mongo
+		products = (items.find({ 
+						'key': { '$regex' :".*" + keyword + ".*"}
+						}).
+						sort(
+							[(sortBy, int(order))]
+						)
+					)
 
 		self.render("search.html", title="Your search result", products=products)
